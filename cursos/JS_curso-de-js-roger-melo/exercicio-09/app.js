89f0e9655e3ccdeb474f713cb2b9c37850c92828
Jonatan Marques <jonatanmarsv@gmail.com>
2024-07-08 21:04:45 -0300

    exerc 9-8 ok

--- cursos/JS_curso-de-js-roger-melo/exercicio-09/app.js
+++ cursos/JS_curso-de-js-roger-melo/exercicio-09/app.js
@@ -93,11 +93,11 @@ console.log(juntaArrays([1,2,3,4,5],[6,7,8,9,10])) */
     mas com o último item removido.
 */
 
-function removeUltimoItem(array){
+/* function removeUltimoItem(array){
   array.pop() // faz remoção do ultimo item
   return array
 }
-console.log(removeUltimoItem([0,1,2,3,4,5,6]))
+console.log(removeUltimoItem([0,1,2,3,4,5,6])) */
 
 /*
   08
@@ -105,6 +105,20 @@ console.log(removeUltimoItem([0,1,2,3,4,5,6]))
   - Crie uma função que retorna se o valor passado como argumento em sua  
     invocação é null.
 */
+
+function seEnull(x) {
+  let argumento = "";
+  if (x === null) {
+    argumento = x+", sim é null";
+  } else {
+    argumento = x+", não é null";
+  }
+  return argumento;
+}
+console.log(seEnull(null));
+console.log(seEnull());
+console.log(seEnull('null'));
+console.log(seEnull('hahahah'));
 
 /*
   09
@@ -137,7 +151,7 @@ console.log(removeUltimoItem([0,1,2,3,4,5,6]))
   "O Xº item do array [X, X, X] é X."
 */
 
-const numbers = [1, 2, 3]
+const numbers = [1, 2, 3];
 
 /*
   12
@@ -147,11 +161,11 @@ const numbers = [1, 2, 3]
     criada.
 */
 
-const letters = ['v', 'e', 'p']
-let lettersCopy = []
+const letters = ["v", "e", "p"];
+let lettersCopy = [];
 
 for (let i = 0; i < letters.length; i++) {
-  lettersCopy.push(letters[i])
+  lettersCopy.push(letters[i]);
 }
 
 /*
@@ -172,18 +186,18 @@ for (let i = 0; i < letters.length; i++) {
   </article>
 */
 
-const section = document.querySelector('[data-js="section"]')
+const section = document.querySelector('[data-js="section"]');
 
 const review = [
-  'Eu sempre adorei o filme e quando descobri que tinha o livro também fiquei doido. Demorei um pouco mas acabei comprando e finalmente li \o/.',
-  'O primeiro filme foi baseado nesse livro, porém o livro (como sempre) é muito mais completo, com mais personagens, mais acontecimentos e até mesmo mais dinossauros. Na verdade nesse livro tem coisas do segundo e terceiro filme também, eles mudaram bastante nos filmes, acho que pra ficar mais comercial, e se o filme é bom, o livro é 100 vezes melhor.',
-  'Michael é um ótimo autor, esse sim pesquisa muito antes de escrever um livro, além da história que já prende sua atenção, ele fala bastante de genética (pra explicar como os dinossauros foram criados) e acaba falando um pouco de programação (informática), por causa dos programas avançados e modernos que o parque tinha. E isso foi uma das coisas que eu achei muito legal, ele explica as coisas com gráficos, tabelas, códigos ... enfim, o cara é foda hahaha.',
-  'Recomendo esse livro pra quem curte uma boa história de ficção. Apesar de muita gente pensar que o livro não tem graça, porque o legal mesmo é ver o dinossauro no filme, com todos os efeitos especiais, eu digo pra deixar esse pensamento de lado, pois a história é tão bem contada e os detalhes são tão bem relatados, que você passa a fazer parte da história, e vive todas as emoções hahaha.'
-]
+  "Eu sempre adorei o filme e quando descobri que tinha o livro também fiquei doido. Demorei um pouco mas acabei comprando e finalmente li o/.",
+  "O primeiro filme foi baseado nesse livro, porém o livro (como sempre) é muito mais completo, com mais personagens, mais acontecimentos e até mesmo mais dinossauros. Na verdade nesse livro tem coisas do segundo e terceiro filme também, eles mudaram bastante nos filmes, acho que pra ficar mais comercial, e se o filme é bom, o livro é 100 vezes melhor.",
+  "Michael é um ótimo autor, esse sim pesquisa muito antes de escrever um livro, além da história que já prende sua atenção, ele fala bastante de genética (pra explicar como os dinossauros foram criados) e acaba falando um pouco de programação (informática), por causa dos programas avançados e modernos que o parque tinha. E isso foi uma das coisas que eu achei muito legal, ele explica as coisas com gráficos, tabelas, códigos ... enfim, o cara é foda hahaha.",
+  "Recomendo esse livro pra quem curte uma boa história de ficção. Apesar de muita gente pensar que o livro não tem graça, porque o legal mesmo é ver o dinossauro no filme, com todos os efeitos especiais, eu digo pra deixar esse pensamento de lado, pois a história é tão bem contada e os detalhes são tão bem relatados, que você passa a fazer parte da história, e vive todas as emoções hahaha.",
+];
 
-let paragraphs = ''
+let paragraphs = "";
 
-section.innerHTML = paragraphs
+section.innerHTML = paragraphs;
 
 /*
   14
